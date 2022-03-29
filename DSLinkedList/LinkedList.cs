26c111d3456f5a3be221c18fcd04b6f431adbc64
iamgauraav <iamgauraav@gmail.com>
2022-03-29 13:59:03 +0530

    Added UC4 : Inserting elements in middle of linked list

--- DSLinkedList/LinkedList.cs
+++ DSLinkedList/LinkedList.cs
@@ -37,6 +37,90 @@ namespace DataStructureProblems
             }
         }
 
+        public void InsertMid(Gtype data, int pos)
+        {
+            //variable
+            //created temp variable
+            Node<Gtype> temp_node = head;
+            int size = Size();
+            // Checking limitation --->can not go maximum size(size + 1 ) or less than zero position
+            if (pos > size + 1 || pos < 1)
+            {
+                Console.WriteLine("Invalid position");
+                return;
+            }
+
+            //create node
+            Node<Gtype> node = new Node<Gtype>(data);
+
+            if (head == null)
+            {
+                Console.WriteLine("List is Empty");
+                if (pos == 1)
+                {
+                    head = node;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid position");
+                }
+            }
+            else
+            {
+                //if you want to make the element at first only means head
+                if (pos == 1)
+                {
+                    //linking element
+                    node.next = head;
+                    head = node;
+                }
+                else
+                {
+                    //count 2 because we have head position as 1 so count start from 2
+                    int count = 2;
+                    //go upto the position where we want to add
+                    //if we want to other than head than
+                    //so less than position
+                    while (pos > count)
+                    {
+                        temp_node = temp_node.next;
+                        count++;
+                    }
+                    //made a new temp node to store unlinked elemaent
+                    Node<Gtype> temp_node2 = temp_node.next;
+                    temp_node.next = node;
+                    node.next = temp_node2;
+                }
+            }
+
+
+        }
+
+        public int Size()
+        {
+            if (head is null)
+            {
+                Console.WriteLine("List is Empty..");
+                return 0;
+            }
+            else
+            {
+                //we had head pposition so count start from 1
+                int count = 1;
+                //created temp variable
+                //the node start from head
+                Node<Gtype> temp_node = head;
+                //display upto ---> node is not equal to null
+                while (temp_node.next != null)
+                {
+                    count++;
+                    Console.WriteLine(temp_node.next.value);
+                    //go upto next element otherwise the loop will always take head
+                    temp_node = temp_node.next;
+                }
+                return count;
+            }
+        }
         public void Display()
         {
             if (head is null)
--- DSLinkedList/Program.cs
+++ DSLinkedList/Program.cs
@@ -7,17 +7,21 @@ namespace DataStructureProblems
 /// </summary>
     class Program
     {/// <summary>
-     /// UC3 - Inserting elements by appending in  linked list
+     /// UC4 - Inserting elements in middle of linked list
      /// </summary>
 
         static void Main(string[] args)
         {
             LinkedList<int> myList = new LinkedList<int>();
             myList.Add(56);
-            myList.Add(30);
             myList.Add(70);
-
+            Console.WriteLine("Linked list before Insertion:");
             myList.Display();
+            myList.InsertMid(30, 2);
+            Console.WriteLine("Linked list after Insertion:");
+            myList.Display();
+
+
         }
     }
 }
